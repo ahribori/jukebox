536e04d9d0687d9900363fdb0f636ada77ae7aa3
Hyun Seung Jeong <ahribori@gmail.com>
2018-03-07 09:56:21 +0900

    Anti adblock disable

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
-import AntiAdblock from './AntiAdblock';
+// import AntiAdblock from './AntiAdblock';
 import registerServiceWorker from './registerServiceWorker';
 
 const pageView = () => {
@@ -14,9 +14,10 @@ const pageView = () => {
     }
 };
 
-ReactDOM.render(
-    <AntiAdblock>
-        <App onLoad={pageView} />
-    </AntiAdblock>
-    , document.getElementById('root'));
+ReactDOM.render(<App onLoad={pageView} />, document.getElementById('root'));
+// ReactDOM.render(
+//     <AntiAdblock>
+//         <App onLoad={pageView} />
+//     </AntiAdblock>
+//     , document.getElementById('root'));
 registerServiceWorker();
